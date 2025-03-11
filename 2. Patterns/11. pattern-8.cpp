991f2702e82a9487c2afd87a159b8aa290518cd9
Balaji-Nandure <balajirn.2016@gmail.com>
2025-03-11 07:48:03 +0530

    New pattern started

---
+++ 2. Patterns/11. pattern-8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    // Solid square pattern 
+    int N = 5;
+
+    for (int i = 0; i<N; i++){
+        for (int j = 0; j<N; j++){
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+} 
