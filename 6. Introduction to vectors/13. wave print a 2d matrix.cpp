51deee63b7a0387cb4e602cfacae5d2c2e389f57
Balaji-Nandure <balajirn.2016@gmail.com>
2025-08-02 16:16:04 +0530

    Woow.. All the types of waveprinting done. Maja hi aa gaya.

--- 6. Introduction to vectors/13. wave print a 2d matrix.cpp
+++ 6. Introduction to vectors/13. wave print a 2d matrix.cpp
@@ -7,40 +7,69 @@ void wavePrint(vector<vector<int>> &v){
     int cols = v[0].size();
 
     // this also works, but for practice lets solve below code.
-    for(int j=0; j<cols; j++){
-        if(j%2 == 0){ //even
-            for(int i=rows-1; i>=0; i--){
-                cout << v[i][j] << " ";
-            }
-        }else{ // odd
-            for(int i  =0; i<rows; i++){
-                cout << v[i][j]<< " ";
+    // for(int j=0; j<cols; j++){
+    //     if(j%2 == 0){ //even
+    //         for(int i=rows-1; i>=0; i--){
+    //             cout << v[i][j] << " ";
+    //         }
+    //     }else{ // odd
+    //         for(int i  =0; i<rows; i++){
+    //             cout << v[i][j]<< " ";
+    //         }
+    //     }
+    // }
+
+    // wavepriinting 2 .
+
+    // for(int j=cols-1; j>=0; j--){
+    //     if(j%2 == 0){ //even
+    //         for(int i=rows-1; i>=0; i--){
+    //             cout << v[i][j] << " ";
+    //         }
+    //     }else{ // odd
+    //         for(int i  =0; i<rows; i++){
+    //             cout << v[i][j]<< " ";
+    //         }
+    //     }
+    // }
+
+    // // wavepriinting 3 by keeping row maving slowly.
+
+    // for(int i=0; i<rows; i++){
+    //     if(i%2 == 0){
+    //         for(int j=0; j<cols; j++){
+    //             cout << v[i][j] << " ";
+    //         }
+    //     }else{
+    //         for(int j=cols-1; j>=0; j--){
+    //             cout << v[i][j] << " ";
+    //         }
+    //     }
+    // }
+
+    // wavepriinting 4.
+
+    for(int i=rows-1; i>=0; i--){
+        if(i%2 == 0){
+            for(int j=cols-1; j>=0; j--){
+                cout<< v[i][j] << " ";
+            } 
+        } else {
+            for(int j=0; j<cols; j++){
+                cout<< v[i][j] << " ";
             }
         }
     }
 
 
-    // Great this approach wont work for all the inputs 
-    // always use above approach.
-    // for(int i=0; i<rows; i++){
-    //     if(i%2 == 1){
-    //         for(int j=0; j<cols; j++){
-    //             cout << v[j][i] << " ";
-    //         }
-    //     }
-    //     else{
-    //         for(int j=cols-1; j>=0; j--){
-    //             cout << v[j][i] << " ";
-    //         }
-    //     }
-    // }
+    
 }
 
 int main(){
     vector<vector<int>> v = {
-        {1,2,3,4}, 
-        {5,6,7,8}, 
-        {9,10,11,12} 
+        {1,2,3}, 
+        {4,5,6}, 
+        {7,8,9} 
     };
 
     wavePrint(v);
